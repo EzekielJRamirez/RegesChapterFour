e6823770bf257cb51eeb8fd5ae250b1f24b721ce
Ezekiel Ramirez <ezekiel.ramirez@calbaptist.edu>
2021-07-16 14:22:10 -0700

    Finished 4.1

--- src/src/main.java
+++ src/src/main.java
@@ -5,5 +5,63 @@
  * git push
  */
 
+import java.util.*;
+
 public class main {
+
+    /* 4.1 page 239*/
+//    public static void main(String[] args) {
+//        /*
+//        PSUEDO CODE
+//        if (currentScore > maxScore) {
+//            System.out.println("A new high score!");
+//            maxScore = currentScore;
+//        }
+//         */
+//
+//        double number = 81;
+//        double answer = 0;
+//        if (number >= 0) {
+//            answer = Math.sqrt(number);
+//        } else {
+//            answer = -1;
+//        }
+//        System.out.println("the square root of number is " + answer);
+//
+//        if (number > 0) {
+//            System.out.println("Number is positive. (" + number + ")");
+//        } else if (number == 0) {
+//            System.out.println("Number is zero.");
+//        } else { // if number is not positive or zero, then it has to be negative
+//            System.out.println("Number is negative. (" + number + ")");
+//        }
+//
+//        // RELATIONAL OPERATORS
+//        // equal to ==
+//        // not equal to !=
+//        // less than <
+//        // greater than >
+//        // less or equal to <=
+//        // greater or equal to >=
+//
+//        // MULTIPLE TEST CONDITIONS
+//        // OR (||)
+//        // AND (&&)
+//        // if (this || that)
+//        // if (this && that)
+//
+//        if (number >= 50 && number <= 100) {
+//            System.out.println("This value (" + number + ") is between 50 and 100");
+//        }
+//    }
+
+    /* 4.2 page 254 */
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+    /* master copy */
+//    public static void main(String[] args) {
+//        System.out.println("hello world!");
+//    }
 }
